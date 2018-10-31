d4ad16f9032633ad8a2d20242bfba9dd74b7928a
Andrei Kniazev <akniazev@gmail.com>
2018-10-31 11:07:25 -0700

    Add min / max freq

--- freq_monitor.py
+++ freq_monitor.py
@@ -84,6 +84,25 @@ class Application(ttk.Frame):
         self.ec0curfreq = ttk.Entry(block, width=9, textvariable=self.c0curfreqVar)
         self.ec0curfreq.pack(side=tk.LEFT, padx=3, pady=3)
 
+        ### Min Frequency
+        block = ttk.Frame(self.cpu0, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Min:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c0minfreqVar = tk.IntVar()
+        self.ec0minfreq = ttk.Entry(block, width=9, textvariable=self.c0minfreqVar)
+        self.ec0minfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Max Frequency
+        block = ttk.Frame(self.cpu0, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Max:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c0maxfreqVar = tk.IntVar()
+        self.ec0maxfreq = ttk.Entry(block, width=9, textvariable=self.c0maxfreqVar)
+        self.ec0maxfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+
 
         ## CPU1 --------------------------------##
         self.cpu1 = ttk.Frame(relief=tk.RIDGE)
@@ -97,6 +116,25 @@ class Application(ttk.Frame):
         self.c1curfreqVar = tk.StringVar()
         self.ec1curfreq = ttk.Entry(block, width=9, textvariable=self.c1curfreqVar)
         self.ec1curfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Min Frequency
+        block = ttk.Frame(self.cpu1, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Min:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c1minfreqVar = tk.IntVar()
+        self.ec1minfreq = ttk.Entry(block, width=9, textvariable=self.c1minfreqVar)
+        self.ec1minfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Max Frequency
+        block = ttk.Frame(self.cpu1, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Max:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c1maxfreqVar = tk.IntVar()
+        self.ec1maxfreq = ttk.Entry(block, width=9, textvariable=self.c1maxfreqVar)
+        self.ec1maxfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
 
 
         ## CPU2 --------------------------------##
@@ -112,6 +150,25 @@ class Application(ttk.Frame):
         self.ec2curfreq = ttk.Entry(block, width=9, textvariable=self.c2curfreqVar)
         self.ec2curfreq.pack(side=tk.LEFT, padx=3, pady=3)
 
+        ### Min Frequency
+        block = ttk.Frame(self.cpu2, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Min:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c2minfreqVar = tk.IntVar()
+        self.ec2minfreq = ttk.Entry(block, width=9, textvariable=self.c2minfreqVar)
+        self.ec2minfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Max Frequency
+        block = ttk.Frame(self.cpu2, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Max:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c2maxfreqVar = tk.IntVar()
+        self.ec2maxfreq = ttk.Entry(block, width=9, textvariable=self.c2maxfreqVar)
+        self.ec2maxfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+
 
         ## CPU3 --------------------------------##
         self.cpu3 = ttk.Frame(relief=tk.RIDGE)
@@ -125,6 +182,25 @@ class Application(ttk.Frame):
         self.c3curfreqVar = tk.StringVar()
         self.ec3curfreq = ttk.Entry(block, width=9, textvariable=self.c3curfreqVar)
         self.ec3curfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Min Frequency
+        block = ttk.Frame(self.cpu3, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Min:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c3minfreqVar = tk.IntVar()
+        self.ec3minfreq = ttk.Entry(block, width=9, textvariable=self.c3minfreqVar)
+        self.ec3minfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Max Frequency
+        block = ttk.Frame(self.cpu3, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Max:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c3maxfreqVar = tk.IntVar()
+        self.ec3maxfreq = ttk.Entry(block, width=9, textvariable=self.c3maxfreqVar)
+        self.ec3maxfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
 
 
         ## CPU4 --------------------------------##
@@ -140,6 +216,25 @@ class Application(ttk.Frame):
         self.ec4curfreq = ttk.Entry(block, width=9, textvariable=self.c4curfreqVar)
         self.ec4curfreq.pack(side=tk.LEFT, padx=3, pady=3)
 
+        ### Min Frequency
+        block = ttk.Frame(self.cpu4, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Min:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c4minfreqVar = tk.IntVar()
+        self.ec4minfreq = ttk.Entry(block, width=9, textvariable=self.c4minfreqVar)
+        self.ec4minfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Max Frequency
+        block = ttk.Frame(self.cpu4, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Max:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c4maxfreqVar = tk.IntVar()
+        self.ec4maxfreq = ttk.Entry(block, width=9, textvariable=self.c4maxfreqVar)
+        self.ec4maxfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+
 
         ## CPU5 --------------------------------##
         self.cpu5 = ttk.Frame(relief=tk.RIDGE)
@@ -153,6 +248,25 @@ class Application(ttk.Frame):
         self.c5curfreqVar = tk.StringVar()
         self.ec5curfreq = ttk.Entry(block, width=9, textvariable=self.c5curfreqVar)
         self.ec5curfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Min Frequency
+        block = ttk.Frame(self.cpu5, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Min:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c5minfreqVar = tk.IntVar()
+        self.ec5minfreq = ttk.Entry(block, width=9, textvariable=self.c5minfreqVar)
+        self.ec5minfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
+        ### Max Frequency
+        block = ttk.Frame(self.cpu5, relief=tk.GROOVE)
+        block.pack(side=tk.LEFT, padx=5, pady=5)
+
+        ttk.Label(block, text="Max:").pack(side=tk.LEFT, padx=3, pady=3)
+        self.c5maxfreqVar = tk.IntVar()
+        self.ec5maxfreq = ttk.Entry(block, width=9, textvariable=self.c5maxfreqVar)
+        self.ec5maxfreq.pack(side=tk.LEFT, padx=3, pady=3)
+
 
 
         ## Browser -----------------------------##
@@ -186,23 +300,49 @@ class Application(ttk.Frame):
             now = datetime.now()
             self.nowVar.set(now.strftime("%H:%M:%S"))
 
+            # Cur / Min / Max
+            # 0
             with open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq') as device:
                 self.c0curfreqVar.set(device.read().replace('\n', ''))
-
+            with open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq') as device:
+                self.c0minfreqVar.set(device.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq') as device:
+                self.c0maxfreqVar.set(device.read().replace('\n', ''))
+            # 1
             with open('/sys/devices/system/cpu/cpu1/cpufreq/scaling_cur_freq') as device:
                 self.c1curfreqVar.set(device.read().replace('\n', ''))
-
+            with open('/sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq') as device:
+                self.c1minfreqVar.set(device.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq') as device:
+                self.c1maxfreqVar.set(device.read().replace('\n', ''))
+            # 2
             with open('/sys/devices/system/cpu/cpu2/cpufreq/scaling_cur_freq') as device:
                 self.c2curfreqVar.set(device.read().replace('\n', ''))
-
+            with open('/sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq') as device:
+                self.c2minfreqVar.set(device.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq') as device:
+                self.c2maxfreqVar.set(device.read().replace('\n', ''))
+            # 3
             with open('/sys/devices/system/cpu/cpu3/cpufreq/scaling_cur_freq') as device:
                 self.c3curfreqVar.set(device.read().replace('\n', ''))
-
+            with open('/sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq') as device:
+                self.c3minfreqVar.set(device.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq') as device:
+                self.c3maxfreqVar.set(device.read().replace('\n', ''))
+            # 4
             with open('/sys/devices/system/cpu/cpu4/cpufreq/scaling_cur_freq') as device:
                 self.c4curfreqVar.set(device.read().replace('\n', ''))
-
+            with open('/sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq') as device:
+                self.c4minfreqVar.set(device.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq') as device:
+                self.c4maxfreqVar.set(device.read().replace('\n', ''))
+            # 5
             with open('/sys/devices/system/cpu/cpu5/cpufreq/scaling_cur_freq') as device:
                 self.c5curfreqVar.set(device.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/cpu5/cpufreq/scaling_min_freq') as device:
+                self.c5minfreqVar.set(device.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/cpu5/cpufreq/scaling_max_freq') as device:
+                self.c5maxfreqVar.set(device.read().replace('\n', ''))
 
 
 
