a6da9144a850820e58c0614c6f9c5d1cbb687fb3
Andrei Kniazev <akniazev@gmail.com>
2018-10-30 17:38:12 -0700

    Add online fields

--- freq_monitor.py
+++ freq_monitor.py
@@ -23,12 +23,21 @@ class Application(ttk.Frame):
         self.info = ttk.Frame(relief=tk.RIDGE)
         self.info.pack(fill=tk.X, expand=1)
         
+        ### Present
         self.lpresent = ttk.Label(self.info, text="Present Cores:")
         self.lpresent.grid(row=0, column=0, padx=10, pady=10)
 
         self.presentVar = tk.StringVar()
-        self.epresent = ttk.Entry(self.info, textvariable=self.presentVar)
+        self.epresent = ttk.Entry(self.info, width=5, textvariable=self.presentVar)
         self.epresent.grid(row=0, column=1, padx=10, pady=10)
+
+        ### Online
+        self.lonline = ttk.Label(self.info, text="Online Cores:")
+        self.lonline.grid(row=0, column=2, padx=10, pady=10)
+
+        self.onlineVar = tk.StringVar()
+        self.eonline = ttk.Entry(self.info, width=5, textvariable=self.onlineVar)
+        self.eonline.grid(row=0, column=3, padx=10, pady=10)
 
         self.start_updates()
 
@@ -38,8 +47,10 @@ class Application(ttk.Frame):
 
     def update_status(self):
         while True:
-            with open('/sys/devices/system/cpu/present') as present:
-                self.presentVar.set(present.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/present') as device:
+                self.presentVar.set(device.read().replace('\n', ''))
+            with open('/sys/devices/system/cpu/online') as device:
+                self.onlineVar.set(device.read().replace('\n', ''))
             sleep(1)
 
 
@@ -48,7 +59,7 @@ class Application(ttk.Frame):
 def main():
     root = tk.Tk()
     app = Application()
-    root.geometry("250x100+900+600")
+  #  root.geometry("250x100+900+600")
     root.mainloop()
 
 
